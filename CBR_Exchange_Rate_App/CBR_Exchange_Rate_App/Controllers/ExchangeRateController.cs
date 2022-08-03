e991410882cdf7b84c83d0572c51bb92d59b1e2a
bruh-moment00 <87185750+bruh-moment00@users.noreply.github.com>
2022-08-03 19:58:17 +0300

    Created blank controller

---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App/Controllers/ExchangeRateController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CBR_Exchange_Rate_App.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeRateController : ControllerBase
+    {
+    }
+}
