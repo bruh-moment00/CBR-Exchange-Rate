b22d5f99f150f45183fe5a44392a6ec203b40053
bruh-moment00 <87185750+bruh-moment00@users.noreply.github.com>
2022-08-03 20:48:13 +0300

    Created generic classes for response

---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App.Data/Models/Interfaces/IResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBR_Exchange_Rate_App.Data.Models.Interfaces
+{
+    public interface IResponse<TSource>
+    {
+        IEnumerable<TSource> Response { get; set; }
+        int TotalCount { get; }
+    }
+}
---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App.Data/Pagination/PaginatedResponse.cs
@@ -0,0 +1,28 @@
+﻿using CBR_Exchange_Rate_App.Data.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBR_Exchange_Rate_App.Domain.Pagination
+{
+    public class PaginatedResponse<TSource>
+    {
+        int PageSize { get; set; }
+        int PageIndex { get; set; }
+        int TotalCount { get; }
+        int PagesCount { get; }
+        IResponse<TSource> Response { get; }
+
+        public PaginatedResponse(int pageSize, int pageIndex, IResponse<TSource> response)
+        {
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            Response = response;
+            TotalCount = response.TotalCount;
+            PagesCount = Convert.ToInt16(Math.Ceiling(Convert.ToDouble(TotalCount) / Convert.ToDouble(PageSize)));
+        }
+
+    }
+}
