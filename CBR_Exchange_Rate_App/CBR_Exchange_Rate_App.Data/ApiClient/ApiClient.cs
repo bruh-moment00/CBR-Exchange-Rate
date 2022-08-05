fad0ee31a1d9ac3ad1dbe2c9a196d15ccb6bbbe2
bruh-moment00 <87185750+bruh-moment00@users.noreply.github.com>
2022-08-05 19:54:02 +0300

    Added API Client

---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App.Data/ApiClient/ApiClient.cs
@@ -0,0 +1,30 @@
+﻿using CBR_Exchange_Rate_App.Data.ApiClient.Interfaces;
+using Leaf.xNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBR_Exchange_Rate_App.Data.ApiClient
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly string _apiUrl;
+        internal ApiClient(string apiUrl)
+        {
+            _apiUrl = apiUrl;
+        }
+
+        string IApiClient.Get(string path)
+        {
+            using (var request = new HttpRequest())
+            {
+                request.UserAgent = Http.ChromeUserAgent();
+
+                HttpResponse httpResponse = request.Get(_apiUrl + path);
+                return httpResponse.ToString();
+            }
+        }
+    }
+}
---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App.Data/ApiClient/Interfaces/IApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBR_Exchange_Rate_App.Data.ApiClient.Interfaces
+{
+    public interface IApiClient
+    {
+        public string Get(string path);
+    }
+}
