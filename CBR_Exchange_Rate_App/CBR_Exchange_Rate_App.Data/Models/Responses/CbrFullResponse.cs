1eaac7eca56958cb0ac6ddad31831a5b7e900a6e
bruh-moment00 <87185750+bruh-moment00@users.noreply.github.com>
2022-08-05 19:55:03 +0300

    Created response models

---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App.Data/Models/Responses/CbrFullResponse.cs
@@ -0,0 +1,35 @@
+﻿using CBR_Exchange_Rate_App.Data.Models.ResponseObjects;
+using CBR_Exchange_Rate_App.Data.Models.ResponseObjects.Interfaces;
+using CBR_Exchange_Rate_App.Data.Models.Responses.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBR_Exchange_Rate_App.Data.Models.Responses
+{
+    public class CbrFullResponse : IResponse
+    {
+        DateTime Date { get; set; }
+        DateTime PreviousDate { get; set; }
+        string PreviousUrl { get; set; }
+        DateTime TimeStamp { get; set; }
+        IDictionary<string, CbrResponseObject> Valute { get; set; }
+        public int TotalCount { get; }
+        public CbrFullResponse(DateTime date, DateTime previousDate, string previousUrl, DateTime timeStamp, IDictionary<string, CbrResponseObject> valute)
+        {
+            Date = date;
+            PreviousDate = previousDate;
+            PreviousUrl = previousUrl;
+            TimeStamp = timeStamp;
+            Valute = valute;
+            TotalCount = valute.Count;
+        }
+
+        public IEnumerable<IResponseObject> GetResponseObjects()
+        {
+            return Valute as IEnumerable<CbrResponseObject>;
+        }
+    }
+}
---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App.Data/Models/Responses/Interfaces/IResponse.cs
@@ -0,0 +1,15 @@
+﻿using CBR_Exchange_Rate_App.Data.Models.ResponseObjects.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBR_Exchange_Rate_App.Data.Models.Responses.Interfaces
+{
+    public interface IResponse
+    {
+        int TotalCount { get; }
+        IEnumerable<IResponseObject> GetResponseObjects();
+    }
+}
