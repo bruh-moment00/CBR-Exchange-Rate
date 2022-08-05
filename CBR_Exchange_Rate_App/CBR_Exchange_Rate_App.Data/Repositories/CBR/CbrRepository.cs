d8c559eef0d1ffac0e74d1dda51b51be0bcfcf61
bruh-moment00 <87185750+bruh-moment00@users.noreply.github.com>
2022-08-05 19:56:00 +0300

    Created repository for retrieving data into model

---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App.Data/Repositories/CBR/CbrRepository.cs
@@ -0,0 +1,25 @@
+﻿using CBR_Exchange_Rate_App.Data.ApiClient.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using CBR_Exchange_Rate_App.Data.Models.Responses;
+
+namespace CBR_Exchange_Rate_App.Data.Repositories.CBR
+{
+    public class CbrRepository : ICbrRepository
+    {
+        private readonly IApiClient _apiClient;
+        public CbrRepository(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public CbrFullResponse getDataFromApi()
+        {
+            return JsonConvert.DeserializeObject<CbrFullResponse>(_apiClient.Get(""));
+        }
+    }
+}
---
+++ CBR_Exchange_Rate_App/CBR_Exchange_Rate_App.Data/Repositories/CBR/ICbrRepository.cs
@@ -0,0 +1,14 @@
+﻿using CBR_Exchange_Rate_App.Data.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBR_Exchange_Rate_App.Data.Repositories.CBR
+{
+    public interface ICbrRepository
+    {
+        public CbrFullResponse getDataFromApi();
+    }
+}
